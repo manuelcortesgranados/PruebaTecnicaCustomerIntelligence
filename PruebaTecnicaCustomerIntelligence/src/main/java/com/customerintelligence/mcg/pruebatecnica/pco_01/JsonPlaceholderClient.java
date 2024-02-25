74069158c3efabd3f02103b295a73148d1c47bac
Manuel Cortés Granados <manuelcortesgranados@gmail.com>
2024-02-25 01:43:54 -0500

    Agregada la clase PostController_2 para manejar solicitudes relacionadas con publicaciones. Este controlador consume el servicio JSONPlaceholder para obtener publicaciones disponibles y proporciona puntos finales para recuperar información sobre ellas. Además, maneja errores de cliente y servidor de manera adecuada, devolviendo los códigos de estado HTTP correspondientes. Autor: Manuel Cortés Granados (manuelcortesgranados@gmail.com), Fecha: 24 de febrero de 2024, 8:00 AM -5:00 GMT Bogotá D.C. Colombia.   Se va incorporando documentación en javadoc.

--- PruebaTecnicaCustomerIntelligence/src/main/java/com/customerintelligence/mcg/pruebatecnica/pco_01/JsonPlaceholderClient.java
+++ PruebaTecnicaCustomerIntelligence/src/main/java/com/customerintelligence/mcg/pruebatecnica/pco_01/JsonPlaceholderClient.java
@@ -1,15 +1,9 @@
-package com.customerintelligence.mcg.pruebatecnica.pco.pco_01;
+package com.customerintelligence.mcg.pruebatecnica.pco_01;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-
-/**
- * @author Manuel Cortés Granados (manuelcortesgranados@gmail.com)
- * @since 24 Feberro 2024 8:00 AM -5:00 GMT Bogotá D.C. Colombia
- *
- */
 
 
 @FeignClient(name = "jsonPlaceholderClient", url = "https://jsonplaceholder.typicode.com")
